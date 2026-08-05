c43353696423d073ef432ec606b864207c88df5e
algemiroteran-spec <jokeros121@gmail.com>
2026-08-05 18:33:01 -0500

    Portfolio Algemiro Teran - Full Stack Developer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,7 @@ import tailwind from "@astrojs/tailwind"
 
 import robotsTxt from "astro-robots-txt"
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://porfolio.dev/'
+  site: 'https://algemiroteran.dev'
 })
