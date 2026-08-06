cca313c1f6fedd4b399b29eb8269a6d186ac888b
algemiroteran-spec <jokeros121@gmail.com>
2026-08-05 19:03:17 -0500

    Update domain to algemiroteran.tech

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,5 @@ import robotsTxt from "astro-robots-txt"
 
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
-  site: 'https://algemiroteran.dev'
+  site: 'https://algemiroteran.tech'
 })
