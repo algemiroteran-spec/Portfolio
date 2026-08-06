28041d306d454b8970a2c1a5e520550f2a16b322
algemiroteran-spec <jokeros121@gmail.com>
2026-08-05 19:22:05 -0500

    SEO completo: sitemap, JSON-LD enriquecido, meta tags, robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config'
 import tailwind from "@astrojs/tailwind"
-
 import robotsTxt from "astro-robots-txt"
+import sitemap from "@astrojs/sitemap"
 
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [
+    tailwind(),
+    robotsTxt({
+      sitemap: true,
+      policy: [
+        { userAgent: '*', allow: '/' },
+      ],
+    }),
+    sitemap(),
+  ],
   site: 'https://algemiroteran.tech'
 })
